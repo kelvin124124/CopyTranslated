4903d0681efb2b19a73bd60b2b2c18d54d52d0ed
kelvin124124 <lindedi03@gmail.com>
2023-09-06 22:14:51 +0800

    update chat message prefix

--- CopyTranslated/Plugin.cs
+++ CopyTranslated/Plugin.cs
@@ -157,7 +157,7 @@ namespace CopyTranslated
         internal void OutputChatLine(SeString message)
         {
             SeStringBuilder sb = new();
-            sb.AddUiForeground("[Item Translated] ", 58).Append(message);
+            sb.AddUiForeground("[CopyTranslated] ", 58).Append(message);
 
             chatGui.PrintChat(new XivChatEntry { Message = sb.BuiltString });
         }
